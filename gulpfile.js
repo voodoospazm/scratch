23a4d89cfe0d0bb6b02ced4fac9272e07bba157d
Roman Spazm <troyzemmann@gmail.com>
2015-11-04 15:32:11 +0300

    Scratch

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+var scss = require('gulp-scss');
+var watch = require('gulp-watch');
+var jade = require('gulp-jade');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
+var spritesmith = require('gulp.spritesmith');
+
+var paths = {
+	scss: 'app/markup/_templates/main.scss',
+	jade: 'app/markup/pages/*.jade'
+};
+
+gulp.task('serve', function() {
+  gulp.src('app')
+  	.pipe(plumber())
+    .pipe(server({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('scss', function() {
+	gulp.src(paths.scss)
+	.pipe(plumber())
+	.pipe(scss({'bundleExec': false}))
+	.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('jade', function() {
+	gulp.src(paths.jade)
+	.pipe(plumber())
+	.pipe(jade({
+		pretty: '\t'
+	}))
+	.pipe(gulp.dest('app/'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch(paths.jade, ['jade']);
+	gulp.watch(paths.scss, ['scss']);
+});
